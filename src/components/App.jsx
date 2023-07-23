eb1c6a28c10ee4cea0f7500347338fa8c4100248
Helena-74 <122282363+Helena-74@users.noreply.github.com>
2023-07-23 14:56:51 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
-export const App = () => {
+import React from 'react';
+import Profile from './components/Profile/Profile.jsx';
+import user from './components/Profile/user.json';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
+
+export default App;
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
